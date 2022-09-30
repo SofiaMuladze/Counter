6bb95b19ddd13b2e62982600e71ffe84186908dc
SofiaMuladze <So.Muladze@gmail.com>
2022-09-30 18:04:11 +0200

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-let count = 0;
 
-document.getElementById("minusBtn").onclick = function(){
-    count-=1;
-    document.getElementById("countLabel").innerHTML = count;
+let countLabel = document.createElement("h2");
+let counter = 0;
+countLabel.innerHTML = counter;
+document.getElementById("value").appendChild(countLabel);
+
+let minusBtn = document.createElement("button");
+minusBtn.innerHTML = "-";
+document.getElementById("buttons").appendChild(minusBtn);
+
+let vLine = document.createElement("div");
+vLine.className = "vl";
+document.getElementById("buttons").appendChild(vLine);
+
+let plusBtn = document.createElement("button");
+plusBtn.innerHTML = "+";
+document.getElementById("buttons").appendChild(plusBtn);
+
+let resetBtn = document.createElement("button");
+resetBtn.innerHTML = "reset";
+document.getElementById("reset").appendChild(resetBtn);
+
+plusBtn.addEventListener("click", () => count(1))
+minusBtn.addEventListener("click", () => count(-1))
+resetBtn.addEventListener("click", () => count(0))
+
+function count(value) {
+    if(value == 0) {
+        counter = 0
+    } else {
+        counter = counter + value
+    }
+    countLabel.innerHTML = counter;
 }
 
- document.getElementById("resetBtn").onclick = function(){
-    count=0;
-    document.getElementById("countLabel").innerHTML = count;
-}
-
-document.getElementById("plusBtn").onclick = function(){
-    count+=1;
-    document.getElementById("countLabel").innerHTML = count;
-}
