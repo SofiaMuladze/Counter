20d0847de06725c3ca09a7ed3758d87025902c74
SofiaMuladze <So.Muladze@gmail.com>
2022-10-10 18:40:50 +0200

    updated scipt js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
 
-let countLabel = document.createElement("h2");
 let counter = 0;
-countLabel.innerHTML = counter;
-document.getElementById("value").appendChild(countLabel);
 
-let minusBtn = document.createElement("button");
-minusBtn.innerHTML = "-";
-document.getElementById("buttons").appendChild(minusBtn);
+createElement("h2","counter", counter, "value");
+createElement("button","minusBtn", "-", "buttons");
+createElement("button","plusBtn", "+", "buttons");
+createElement("button","resetBtn", "reset", "reset");
 
-let vLine = document.createElement("div");
-vLine.className = "vl";
-document.getElementById("buttons").appendChild(vLine);
+var wrapper = document.getElementById("wrapper");
+wrapper.onclick = function(event) {
+    let target = event.target; 
+    if (target.id == 'minusBtn') count(-1); 
+    if (target.id == 'plusBtn') count(1); 
+    if (target.id == 'resetBtn') count(0); 
+};
 
-let plusBtn = document.createElement("button");
-plusBtn.innerHTML = "+";
-document.getElementById("buttons").appendChild(plusBtn);
+function createElement(elementTag, elementId, elementValue, appendToElement) {
+    let element = document.createElement(elementTag);
+    element.id = elementId;
+    element.innerHTML = elementValue;
+    document.getElementById(appendToElement).appendChild(element);
+}
 
-let resetBtn = document.createElement("button");
-resetBtn.innerHTML = "reset";
-document.getElementById("reset").appendChild(resetBtn);
-
-plusBtn.addEventListener("click", () => count(1))
-minusBtn.addEventListener("click", () => count(-1))
-resetBtn.addEventListener("click", () => count(0))
 
 function count(value) {
-    if(value == 0) {
+    if (value == 0) {
         counter = 0
     } else {
         counter = counter + value
     }
-    countLabel.innerHTML = counter;
+    document.getElementById("counter").innerHTML = counter;
 }
 
